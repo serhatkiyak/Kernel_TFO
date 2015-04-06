191a3373bf2dc2c83916f53285efa569db822f7c
serhat <serhatkiyak91@gmail.com>
2015-04-05 23:52:58 -0400

    AB benchmark

---
+++ ab/TFO/ab.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+while read SITE
+do
+	mkdir $SITE
+
+	s="/"
+	http="https://"
+	url="$http$SITE$s"
+
+	for i in {10..50..10}
+	do	
+		ab -n $i -c 1 -f TLS1 $url &> $SITE/$i.txt 
+	done
+
+done < tfo.txt
