ae8e4b7087b7caf1e463750571a158abed28d27d
root <serhatkiyak91@gmail.com>
2015-04-28 23:09:56 -0400

    Adding experiment scripts to measure page load time of TCP and TFO

---
+++ wget/scatter.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+
+import csv
+import os.path
+import matplotlib.pyplot as plt
+import pylab
+import numpy as np
+
+N = 32
+
+ctime_tcp = [0] * N
+ctime_tfo = [0] * N
+
+for folder in range(1, N+1): 
+    tcp_total = 0
+    tfo_total = 0
+    tcp_counter = 0
+    tfo_counter = 0
+    for iteration in range(1, 7): 
+        tcp_file_path = str(folder) + '/' + str(iteration) + '/tcp_time.txt'
+	tfo_file_path = str(folder) + '/' + str(iteration) + '/tfo_time.txt'
+        if os.path.exists(tcp_file_path):
+            with open(tcp_file_path,'rb') as tsvin:
+                tsvin = csv.reader(tsvin, delimiter='\t')
+		for row in tsvin:
+		    tcp_total += float(row[0])
+		    tcp_counter = tcp_counter + 1
+        if os.path.exists(tfo_file_path):
+            with open(tfo_file_path,'rb') as tsvin:
+                tsvin = csv.reader(tsvin, delimiter='\t')
+		for row in tsvin:
+		    tfo_total += float(row[0])
+		    tfo_counter = tfo_counter + 1
+    if tcp_counter != 0:
+        ctime_tcp[folder-1] = tcp_total / tcp_counter
+    if tfo_counter != 0:
+        ctime_tfo[folder-1] = tfo_total / tfo_counter	
+
+print ctime_tcp
+print ctime_tfo
+
+xaxis = [0] * N
+for index in range(1, N+1):
+	xaxis[index-1] = index
+
+plt.ylabel('Connection time')
+plt.xlabel('Server')
+##plt.scatter(xaxis, false, label='FALSE', color='red')
+##plt.scatter(xaxis, full, label='FULL', color='green')
+plt.scatter(xaxis, ctime_tcp, label='TCP', color='green')
+plt.scatter(xaxis, ctime_tfo, label='TFO', color='red')
+##plt.plot([-500,2000], [0,0], 'k-', lw=2)
+##plt.plot([-500,2000], [50,50], 'k-', lw=2)
+##plt.ylim(0, 200)
+##plt.xlim(-500, 2000)
+plt.legend(loc = 'lower right')
+plt.show()
